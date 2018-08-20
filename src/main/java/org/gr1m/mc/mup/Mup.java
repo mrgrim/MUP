364f793931e8a2ff4627cd0c7cd60080802ccf27
Michael Kreitzer <mrgrim@gr1m.org>
2018-08-19 21:41:50 -0400

    Convert to semantic versioning format.

--- src/main/java/org/gr1m/mc/mup/Mup.java
+++ src/main/java/org/gr1m/mc/mup/Mup.java
@@ -17,13 +17,14 @@ import java.io.File;
 @Mod(modid = Mup.MODID,
      name = Mup.NAME,
      version = Mup.VERSION,
+     acceptedMinecraftVersions = "1.12.2",
      certificateFingerprint = Mup.FINGERPRINT,
      guiFactory = "org.gr1m.mc.mup.config.MupGuiFactory")
 public class Mup
 {
     public static final String MODID = "mup";
     public static final String NAME = "EigenCraft Unofficial Patch";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.0";
     public static final String FINGERPRINT = "@FINGERPRINT@";
 
     public static Logger logger;
