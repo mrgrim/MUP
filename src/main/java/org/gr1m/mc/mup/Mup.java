12836c16f05be6b0c6e51944e0048d4d92e2a51b
Michael Kreitzer <mrgrim@gr1m.org>
2019-10-21 17:13:20 -0400

    Version 1.3.4

--- src/main/java/org/gr1m/mc/mup/Mup.java
+++ src/main/java/org/gr1m/mc/mup/Mup.java
@@ -29,7 +29,7 @@ public class Mup
 {
     public static final String MODID = "mup";
     public static final String NAME = "EigenCraft Unofficial Patch";
-    public static final String VERSION = "1.3.3";
+    public static final String VERSION = "@VERSION@";
     public static final String FINGERPRINT = "@FINGERPRINT@";
 
     public static Logger logger = LogManager.getLogger();;
