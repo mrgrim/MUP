7399cf9b8e8f306f70c61b08054ed3bf38c933af
Michael Kreitzer <mrgrim@gr1m.org>
2019-11-10 18:37:23 -0500

    Handle end portal case for MC-98153

--- src/main/java/org/gr1m/mc/mup/bugfix/mc98153/mixin/MixinPlayerList.java
+++ src/main/java/org/gr1m/mc/mup/bugfix/mc98153/mixin/MixinPlayerList.java
@@ -1,21 +1,23 @@
 package org.gr1m.mc.mup.bugfix.mc98153.mixin;
 
 import net.minecraft.network.NetHandlerPlayServer;
-import net.minecraft.world.Teleporter;
+import net.minecraft.server.management.PlayerList;
 import org.gr1m.mc.mup.Mup;
 import org.gr1m.mc.mup.bugfix.mc98153.INetHandlerPlayServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(Teleporter.class)
-public abstract class MixinTeleporter
+@Mixin(PlayerList.class)
+public class MixinPlayerList
 {
-    @Redirect(method = "placeInExistingPortal", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetHandlerPlayServer;setPlayerLocation(DDDFF)V"))
+    // Forge specific
+    @Redirect(method = "transferPlayerToDimension(Lnet/minecraft/entity/player/EntityPlayerMP;ILnet/minecraftforge/common/util/ITeleporter;)V",
+              at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetHandlerPlayServer;setPlayerLocation(DDDFF)V", ordinal = 0))
     private void doCapturePlayerLocation(NetHandlerPlayServer connection, double x, double y, double z, float yaw, float pitch)
     {
         connection.setPlayerLocation(x, y, z, yaw, pitch);
-        
+
         if (Mup.config.mc98153.enabled)
         {
             ((INetHandlerPlayServer) connection).callCaptureCurrentPosition();
